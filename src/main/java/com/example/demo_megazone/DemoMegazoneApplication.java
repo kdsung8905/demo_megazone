5f31fb0d37968286f7935a340babe8a606477c8b
soyeon <lsyeon0825@gmail.com>
2023-03-12 11:25:45 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/demo_megazone/DemoMegazoneApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_megazone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMegazoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoMegazoneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demo_megazone/DemoMegazoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_megazone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMegazoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
